1d958e8cbd2c0a50ae7a6ef079fb129830a4ae17
AMagicRake <niel.gow@live.co.uk>
2024-06-22 16:21:36 +0100

    Added test on save and load

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,9 +1,12 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
-//t.Errorf("Expected deck length of 16, but got %v", len(d))
-//test card count
+// t.Errorf("Expected deck length of 16, but got %v", len(d))
+// test card count
 func TestNewDeck(t *testing.T) {
 
 	d := newDeck()
@@ -24,4 +27,23 @@ func TestDeal(t *testing.T) {
 	if len(d) != (deckSize) {
 		t.Errorf("Expected decksize of %v, but got %v", (deckSize), len(d))
 	}
+}
+
+func TestSaveAndLoadDeck(t *testing.T) {
+	fileName := "deckState.test.dck"
+	os.Remove(fileName)
+
+	deck := newDeck()
+	deck.saveToFile(fileName)
+
+	loadedDeck := newDeckFromFile(fileName)
+	if len(deck) != len(loadedDeck) {
+		t.Errorf("Expected decksize of %v, but got %v", len(deck), len(loadedDeck))
+	}
+	for i := range deck {
+		if deck[i] != loadedDeck[i] {
+			t.Errorf("The value of loadedDeck at index %v is not the same as the written deck at the same index.", i)
+		}
+	}
+	os.Remove(fileName)
 }
--- cards/main.go
+++ cards/main.go
@@ -5,7 +5,7 @@ func main() {
 	cardDeck := newDeck()
 	cardDeck.shuffle()
 	cardDeck.print()
-	// cardDeck.saveToFile("deckState.dck")
+	cardDeck.saveToFile("deckState.dck")
 
 	// readDeck := newDeckFromFile("deckState.dck")
 	// readDeck.print()
