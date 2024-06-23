f7b5cc15782865583cfd1c8395f9e1f018eccedc
AMagicRake <niel.gow@live.co.uk>
2024-06-23 11:19:41 +0100

    quick experiments with maps

--- maps/main.go
+++ maps/main.go
@@ -1,5 +1,26 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	// var colors map[string]string
+	// colors := make(map[string]string)
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#00ff00",
+		"blue":  "#0000ff"}
+	colors["white"] = "#ffffff"
+
+	delete(colors, "red")
+
+	fmt.Println(colors)
+
+	printMap(colors)
+}
+
+func printMap(m map[string]string) {
+	for key, value := range m {
+		fmt.Println(key, value)
+	}
 }
