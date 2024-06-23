251622e49c6f8fbc99a59af136d46c8ee2c49c41
AMagicRake <niel.gow@live.co.uk>
2024-06-23 09:28:04 +0100

    Added comms substruct

--- structs/main.go
+++ structs/main.go
@@ -6,18 +6,29 @@ import (
 )
 
 type person struct {
-	FirstName string `json:"firstName"`
-	LastName  string `json:"lastName"`
+	FirstName string      `json:"firstName"`
+	LastName  string      `json:"lastName"`
+	Comms     contactInfo `json:"contactInfo"`
+}
+
+type contactInfo struct {
+	Email        string `json:"emailAddress"`
+	MobileNumber string `json:"mobileNumber"`
 }
 
 func (p person) toString() string {
-	return p.FirstName + " " + p.LastName
+	return p.FirstName + " " + p.LastName + " " + p.Comms.toString()
+}
+
+func (ci contactInfo) toString() string {
+	return ci.Email + " " + ci.MobileNumber
 }
 
 func main() {
 
-	niel := person{"Niel", "Gow"}
+	niel := person{"Niel", "Gow", contactInfo{"niel.gow@domain.com", "+4407123456789"}}
 	meg := person{FirstName: "Megan", LastName: "Smith"}
+	meg.Comms = contactInfo{Email: "meg.smith@domain.com", MobileNumber: "+467123456789"}
 
 	fmt.Println(niel.toString())
 	fmt.Println(meg.toString())
