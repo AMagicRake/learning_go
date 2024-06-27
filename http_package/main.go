9fc7a206bf842a089d43f4adf6f074511c953a3c
AMagicRake <niel.gow@live.co.uk>
2024-06-27 17:29:54 +0100

    experminted with Reader interface

--- http_package/main.go
+++ http_package/main.go
@@ -12,6 +12,11 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	// bs := make([]byte, 99999)
+	// bytes, err := resp.Body.Read(bs)
+	// if bytes > 0 && err != nil {
+	// 	fmt.Println(string(bs))
+	// }
 	respBody, err := io.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatal(err)
